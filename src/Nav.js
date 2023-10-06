a5a1487e26002021970946bd56590886b944514f
Y <techprivacy@gmail.com>
2023-10-06 10:18:56 +0530

    router

--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,16 @@
+import { Link } from 'react-router-dom';
 import './Nav.css'
 
 function Nav() {
   return (
     <>
   <ul>
-
-  <a href='/'> <li>Home</li></a>
+<Link to ='/'><li>Home</li></Link>
+<Link to ='/about'><li>About</li></Link>
+<Link to ='/contact'><li>Contact</li></Link>
+  {/* <a href='/'> <li>Home</li></a>
   <a href='/about'> <li>About</li></a> 
-  <a href='/contact'> <li>Contact</li></a> 
+  <a href='/contact'> <li>Contact</li></a>  */}
   </ul>
   </>
   );
